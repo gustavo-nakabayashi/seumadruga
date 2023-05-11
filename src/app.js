370b36f974eb1f86a82fe79185c57580c95b2ff2
Gustavo Nakabayashi <gustavo@disco-tec.com>
2023-05-10 22:09:16 -0300

    feat: get users route

--- src/app.js
+++ src/app.js
@@ -5,4 +5,9 @@ app.get("/", (req, res) => {
   res.status(200).send();
 });
 
+app.get("/users", (req, res) => {
+  const users = [{ name: "Gustavo Barros", email: "gustavo@email.com" }];
+  res.status(200).json(users);
+});
+
 module.exports = app;
