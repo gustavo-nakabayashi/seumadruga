e21587975d1ac513a048509fd07bae028395a92a
Gustavo Nakabayashi <gustavo@disco-tec.com>
2023-05-10 22:24:23 -0300

    feat: add post create users route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
   res.status(200).send();
 });
@@ -8,6 +10,10 @@ app.get("/", (req, res) => {
 app.get("/users", (req, res) => {
   const users = [{ name: "Gustavo Barros", email: "gustavo@email.com" }];
   res.status(200).json(users);
+});
+
+app.post("/users", (req, res) => {
+  res.status(201).json(req.body);
 });
 
 module.exports = app;
