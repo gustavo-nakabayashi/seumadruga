0427c45fcb2a0720c3589070d794308c28c53028
Gustavo Nakabayashi <gustavo@disco-tec.com>
2023-05-12 18:28:34 -0300

    feat: add knex logger

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+// const knexlogger = require('knex-logger');
+
 module.exports = (app) => {
   app.use(express.json());
+  // app.use(knexlogger(app.db));
 };
