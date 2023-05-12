686c44057d4331b452d12c465ed0686320625f44
Gustavo Nakabayashi <gustavo@disco-tec.com>
2023-05-12 19:42:03 -0300

    feat: create validations for password and email

--- src/services/user.js
+++ src/services/user.js
@@ -3,6 +3,8 @@ module.exports = (app) => {
 
   const create = (body) => {
     if (!body.name) return { error: 'Name is a required attribute' };
+    if (!body.email) return { error: 'Email is a required attribute' };
+    if (!body.password) return { error: 'Password is a required attribute' };
     return app.db('users').insert(body, '*');
   };
 
--- test/routes/users.test.js
+++ test/routes/users.test.js
@@ -21,3 +21,17 @@ test('Should not create a user without name', async () => {
   expect(response.status).toBe(400);
   expect(response.body.error).toBe('Name is a required attribute');
 });
+
+test('Should not create an user without email', async () => {
+  const data = { name: 'Gustavo', password: '123456' };
+  const response = await request(app).post('/users').send(data);
+  expect(response.status).toBe(400);
+  expect(response.body.error).toBe('Email is a required attribute');
+});
+
+test('Should not create an user without password', async () => {
+  const data = { name: 'Gustavo', email: 'gustavo@email.com' };
+  const response = await request(app).post('/users').send(data);
+  expect(response.status).toBe(400);
+  expect(response.body.error).toBe('Password is a required attribute');
+});
