da4009fdb503e8f442dd480b7c3a087a9b5caa4b
Gustavo Nakabayashi <gustavo@disco-tec.com>
2023-05-11 22:16:33 -0300

    test: get users connected to db

--- test/user.test.js
+++ test/user.test.js
@@ -4,8 +4,7 @@ const app = require('../src/app');
 test('Should return user list', async () => {
   const response = await request(app).get('/users');
   expect(response.status).toBe(200);
-  expect(response.body).toHaveLength(1);
-  expect(response.body[0]).toHaveProperty('name', 'Gustavo Barros');
+  expect(response.body.length).toBeGreaterThan(0);
 });
 
 test('Should create a new user', async () => {
