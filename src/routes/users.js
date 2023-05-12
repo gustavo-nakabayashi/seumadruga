a956b1cc11b4c8e6aff6878499762d050dd523e6
Gustavo Nakabayashi <gustavo@disco-tec.com>
2023-05-11 21:53:57 -0300

    feat: make create route create user in db

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,12 @@
-module.exports = () => {
+module.exports = (app) => {
   const findAll = (req, res) => {
     const users = [{ name: 'Gustavo Barros', email: 'gustavo@email.com' }];
     res.status(200).json(users);
   };
 
-  const create = (req, res) => {
-    res.status(201).json(req.body);
+  const create = async (req, res) => {
+    const response = await app.db('users').insert(req.body, '*');
+    res.status(201).json(response[0]);
   };
 
   return { findAll, create };
