96a4f0e22104e238407f92693ca3a6a889d6055a
Gustavo Nakabayashi <gustavo@disco-tec.com>
2023-05-13 07:26:41 -0300

    test: add get all accounts list

--- test/routes/accounts.test.js
+++ test/routes/accounts.test.js
@@ -10,6 +10,12 @@ beforeAll(() => {
   return request(app).post('/users').send(data).then(response => userId = response.body.id);
 });
 
+test('Should return accounts list', async () => {
+  const response = await request(app).get('/accounts');
+  expect(response.status).toBe(200);
+  expect(response.body.length).toBeGreaterThan(0);
+});
+
 test('Should create an account successfully', () => {
   const data = { user_id: userId, name: 'Inter' };
   return request(app).post('/accounts').send(data).then((response) => {
